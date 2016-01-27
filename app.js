47730e3e1e27bcf8d64376c21abd70d055e5e96e
piumosso <golikoffartem@gmail.com>
2016-01-27 12:48:07 +0300

    Innter builds

--- app.js
+++ app.js
@@ -12,13 +12,95 @@ angular
     $scope.blocks = [
       {
         type: 'text',
-        content: '123'
+        content: '1'
       },
       {
         type: 'text',
-        content: '123<br>456<br>789<br>0'
+        content: '2'
+      },
+      {
+        type: 'container',
+        blocks: [
+          {
+            type: 'text',
+            content: '3'
+          },
+          {
+            type: 'text',
+            content: '4'
+          }
+        ]
       }
     ];
 
+  }])
+  .directive('blockCollection', ['RecursionHelper', (RecursionHelper) => ({
+    scope: {
+      blocks: '=',
+      setOuterHoverStatus: '='
+    },
+    templateUrl: 'templates/block-collection.html',
+    controller: ['$scope', $scope => {
+
+      $scope.isHover = false;
+      $scope.isInnerHover = false;
+      $scope.setInnerHoverStatus = (status) => $scope.isInnerHover = status;
+      $scope.isActive = () => {
+        return $scope.isHover && !$scope.isInnerHover;
+      };
+      $scope.mouseenter = () => {
+        $scope.isHover = true;
+        $scope.setOuterHoverStatus && $scope.setOuterHoverStatus(true);
+      };
+      $scope.mouseleave = () => {
+        $scope.isHover = false;
+        $scope.setOuterHoverStatus && $scope.setOuterHoverStatus(false);
+      };
+
+    }],
+    compile: function(element) {
+      return RecursionHelper.compile(element);
+    }
+  })])
+  .factory('RecursionHelper', ['$compile', function($compile){
+    return {
+      /**
+       * Manually compiles the element, fixing the recursion loop.
+       * @param element
+       * @param [link] A post-link function, or an object with function(s) registered via pre and post properties.
+       * @returns An object containing the linking functions.
+       */
+      compile: function(element, link){
+        // Normalize the link parameter
+        if(angular.isFunction(link)){
+          link = { post: link };
+        }
+
+        // Break the recursion loop by removing the contents
+        var contents = element.contents().remove();
+        var compiledContents;
+        return {
+          pre: (link && link.pre) ? link.pre : null,
+          /**
+           * Compiles and re-adds the contents
+           */
+          post: function(scope, element){
+            // Compile the contents
+            if(!compiledContents){
+              compiledContents = $compile(contents);
+            }
+            // Re-add the compiled contents to the element
+            compiledContents(scope, function(clone){
+              element.append(clone);
+            });
+
+            // Call the post-linking function, if any
+            if(link && link.post){
+              link.post.apply(null, arguments);
+            }
+          }
+        };
+      }
+    };
   }]);
 
